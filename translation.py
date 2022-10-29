e6c14bfe9abb7a8ac811efdc4329e761e61830ae
TGDarkLord <113157573+TGDarkLord@users.noreply.github.com>
2022-10-29 22:59:10 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,24 +2,22 @@ class Translation(object):
 
     ERROR = "<b>ERROR :</b> {}"
 
-    START_TEXT = """Hi {},
-I'm URL X Uploader!
-You can upload HTTP/HTTPS direct link, Using this bot!
+    START_TEXT = """<b><i>Hello 👋 {},\n\nI'm URL Uploader Bot!\nYou Can Upload HTTP/HTTPS Direct Link, Using This Bot.\n\n/help For More Details</b></i>"""
 
-/help for more details!"""
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    FORMAT_SELECTION = "Select The Desired Format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
-URL | filename | username | password"""
-    DOWNLOAD_START = "📥Downloading..."
-    UPLOAD_START = "📤Uploading..."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @Uploader_X_bot\n\n<b>Join : @Space_X_Bots</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@Uploader_X_Bot"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+URL | Filename | Username | Password"""
+    DOWNLOAD_START = "📥 Downloading..."
+    UPLOAD_START = "📤 Uploading..."
+    RCHD_TG_API_LIMIT = "Downloaded In {} Seconds.\nDetected File Size: {}\nSorry, But I Cannot Upload Files Greater Than 2GB Due To Telegram API Limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks For Using @TechUrlUploader_Bot\n\n<b>Join : @Tech_Projects2019</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded In {} Seconds.\nUploaded In {} Seconds."
+    SAVED_CUSTOM_THUMB_NAIL = "Custom Video/File Thumbnail Saved. This Image Will Be Used In The Video/File."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom Thumbnail Deleted Successfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
-    HELP_USER = """How to Use Me? Follow These steps!
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> Said: {}"
+    HELP_USER = """How To Use Me? Follow These Steps!
     
 1. Send url (example.domain/File.mp4 | New Filename.mp4).
 2. Send Image As Custom Thumbnail (Optional).
@@ -29,13 +27,14 @@ URL | filename | username | password"""
    Video  - Give File as video without Screenshots
    File   - Give File without Screenshots
 
-If bot didn't respond, contact @Sources_Codes"""
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
-You can use /rename command after receiving file to rename it with custom thumbnail support.
-"""
-    CANCEL_STR = "Process Cancelled"
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+If Bot Didn't Respond, Contact @TechprojectsChat"""
 
-    ERROR_YTDLP = "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output."
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail To A Media Album, To Generate Custom Thumbnail"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album Should Contain Only Two Photos. Please Re-send The Media Album, And Then Try Again Or Send Only Two Photos In An Album."
+You Can Use /rename Command After Receiving File To Rename It With Custom Thumbnail Support."""
+
+    CANCEL_STR = "✔️ Process  Successfully Cancelled"
+    ZIP_UPLOADED_STR = "Uploaded {} Files In {} Seconds."
+    SLOW_URL_DECED = "Gosh That Seems To Be A Very Slow URL. Since You Were Screwing My Home, I Am In No Mood To Download This File. Meanwhile, Why Don't You Try This:==> https://shrtz.me/PtsVnf6 And Get Me A Fast URL So That I Can Upload To Telegram, Without Me Slowing Down For Other Users."
+
+    ERROR_YTDLP = "Please Report This Issue On https://yt-dl.org/bug. Make Sure You Are Using The Latest Version; See ===>> https://yt-dl.org/update On How To Update. Be Sure To Call YouTube-Dl With The Verbose Flag And Include Its Complete Output."
